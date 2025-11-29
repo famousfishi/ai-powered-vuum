4459f9550ff957830a42cd1a7a48d7057f38c867
fisayo-oluwayemi <fisayo@kindredteam.com>
2025-11-29 22:34:23 +0100

    added configuration values

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,14 @@
 import express from 'express';
 import type { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    res.send('Hello from the fisayo server today!');
+    res.send(process.env.OPENAI_API_KEY ? `API Key is set to: ${process.env.OPENAI_API_KEY}` : "test");
 });
 
 app.listen(port, () => {
