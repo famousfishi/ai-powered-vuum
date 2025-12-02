2f82128767dfa65c2aef0494fb6d5646e06c418b
fisayo-oluwayemi <fisayo@kindredteam.com>
2025-12-02 04:45:12 +0100

    added run both apps together

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via fisayo!");
+import concurrently from 'concurrently';
+
+concurrently(
+    [
+        { command: 'bun run dev', name: 'server', prefixColor: 'blue', cwd: 'packages/server' },
+        { command: 'bun run dev', name: 'client', prefixColor: 'green', cwd: 'packages/client' }
+    ])
